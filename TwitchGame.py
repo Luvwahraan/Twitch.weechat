c71e27d3b22c7af0e4365e207cd94ba7178519ed
florian <luvwahraan+github@pikaed.org>
2025-08-16 18:42:21 +0200

    TwitchGame initial commit

---
+++ TwitchGame.py
@@ -0,0 +1,13 @@
+VERSION='0.01'
+
+import weechat
+
+weechat.register( \
+        'TwitchGame', \
+        'Luvwahraan', \
+        VERSION, \
+        'GPLv3', \
+        'A Touiche game', \
+        "", "")
+
+
