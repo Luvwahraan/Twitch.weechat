bf28d4f6d7fbf66e6e8f02f368d69ff9267148a2
florian <luvwahraan+github@pikaed.org>
2025-08-16 18:19:47 +0200

    more cleaning

--- twitch_oauth.py
+++ twitch_oauth.py
@@ -9,14 +9,6 @@ SCRIPT_LICENSE = "Whatever"
 SCRIPT_DESC    = "OAuth Twitch auto update"
 
 
-cfg_refresh_token = "plugins.var.python.twitch_oauth.refresh_token"
-cfg_irc_server =    "plugins.var.python.twitch_oauth.irc_server"
-#cfg_expire_time =   "plugins.var.python.twitch_oauth.expire_time"
-
-#cfg_refresh_token = "irc.server.{irc_server}.twitch_oauth.refresh_token"
-#cfg_irc_server =    "plugins.var.python.twitch_oauth.irc_server"
-#cfg_expire_time =   "plugins.var.python.twitch_oauth.expire_time"
-
 def update_request(irc_server):
     refresh_token = weechat.config_get_plugin( f"{irc_server}.refresh_token" )
     refresh_url = f"https://twitchtokengenerator.com/api/refresh/{refresh_token}"
@@ -27,7 +19,6 @@ def update_request(irc_server):
             data = response.json()
             new_token = data['token']
 
-            #irc_server = weechat.config_string(  weechat.config_get(cfg_irc_server) )
             weechat.command("", f"/set irc.server.{irc_server}.password oauth:{new_token}")
             weechat.command("", f"/save irc")
             #weechat.command("", f"/reconnect {irc_server}")
@@ -37,7 +28,7 @@ def update_request(irc_server):
             next_timer = int((expire_time - 9000) * 1000)  # 9000s = 2h30
             weechat.hook_timer(next_timer, 0, 1, "refresh_oauth_cb", "")
         else:
-            raise Exception(f"Erreur HTTP lors du renouvellement: {response.status_code}")
+            raise Exception(f"Can't refresh toker, HTTP error: {response.status_code}")
     except Exception as e:
         weechat.prnt('', f"[{SCRIPT_NAME}] {e}")
         weechat.hook_timer(60000, 0, 1, "refresh_oauth_cb", "") # retries after 60s
@@ -49,7 +40,7 @@ def addserver_command_cb(data, buffer, args):
     args_list = args.split(' ')
     if len(args_list) > 2:
         weechat.prnt('', f"[{SCRIPT_NAME}] Bad args.")
-        weechat.prnt('', f"[{SCRIPT_NAME}] /{SCRIPT_NAME} server refresh_token")
+        weechat.prnt('', f"[{SCRIPT_NAME}] /{SCRIPT_NAME} servername refresh_token")
         return weechat.WEECHAT_RC_OK
 
     irc_server = args_list[0]
@@ -93,15 +84,14 @@ if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
     script_options = {
         'irc_servers':  '',
         'expire_time':  "5184000",
-        #cfg_refresh_token:  "please set refresh token",
-        #cfg_irc_server:     "twitch",
     }
     for option, default_value in script_options.items():
         if not weechat.config_get_plugin(option):
             weechat.config_set_plugin(option, default_value)
 
     weechat.hook_timer( 1500, 0, 1, "refresh_oauth_cb", "" )
-    weechat.hook_command( SCRIPT_NAME, 'Add update token for a twitch server.', 
+    weechat.hook_command( SCRIPT_NAME,
+            'Add update token for a twitch server.', 
             '', '', '',
             'addserver_command_cb', '' )
 
